cce3436131dcef115be864928bd2aa6f47b38850
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-06-26 20:34:04 +0530

    Time: 5 ms (15.38%) | Memory: 10.1 MB (77.15%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+//lee215 typa solution
+int sum=0;
+    TreeNode* helper(TreeNode* root) {
+      if(root->right)helper(root->right);
+        sum+=root->val;
+        root->val=sum;
+        if(root->left)helper(root->left);
+        return root;
+
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        helper(root);
+        return root;
+    }
+};
