9edaf79b9fde9af2141b94fe1de1d6abe2836108
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-06-30 23:42:26 +0530

    Time: 115 ms (73.74%) | Memory: 67.2 MB (72.21%) - LeetSync

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void dfs(int i, int j, vector<vector<int>>& ed, vector<vector<int>>& ans,
+             vector<bool>& vis) {
+        vis[i] = true;
+        for (int itr : ed[i]) {
+            if (!vis[itr]) {
+                ans[itr].push_back(j);
+                dfs(itr, j, ed, ans, vis);
+            }
+        }
+    }
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> ed(n);
+        vector<vector<int>> ans(n);
+        for (auto itr : edges) {
+            ed[itr[0]].push_back(itr[1]);
+        }
+        for (int i = 0; i < n; i++) {
+            vector<bool> vis(n, false);
+            dfs(i, i, ed, ans, vis);
+        }
+        return ans;
+    }
+};
