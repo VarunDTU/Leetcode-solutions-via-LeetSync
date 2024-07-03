2b68e2a31c16510f6b8affdb754ff47ed03ca121
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-07-03 20:44:43 +0530

    Time: 7 ms (62.91%) | Memory: 8.2 MB (88.40%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string match = "";
+        while (match.size() < b.size())
+            match += a;
+        cout << match;
+
+        if (match.find(b) != string::npos)
+            return match.size() / a.size();
+
+        match += a;
+        if (match.find(b) != string::npos)
+            return (match.size() / a.size());
+
+        return -1;
+    }
+};
