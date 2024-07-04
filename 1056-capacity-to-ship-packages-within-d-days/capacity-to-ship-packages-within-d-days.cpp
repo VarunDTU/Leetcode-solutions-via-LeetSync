ee9981294397404ae2162bec8cada541c831eb96
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-07-04 20:06:07 +0530

    Time: 38 ms (68.54%) | Memory: 33.6 MB (31.96%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // min :max(weights)
+    // max:sum(weights)
+    // O(n*(max-min))
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int minW = INT_MIN, maxW = 0;
+        int ans = 0;
+        for (auto itr : weights) {
+            minW = max(itr, minW);
+            maxW += itr;
+        }
+
+        // s->minW, end->maxW
+        while (minW <= maxW) {
+            int midW = minW + (maxW - minW) / 2;
+            int currentdays = 1, current = 0;
+
+            for (int w : weights) {
+                current += w;
+                if (current > midW) {
+                    currentdays++;
+                    current = w;
+                }
+            }
+
+            if (currentdays > days)
+                minW = midW + 1;
+            else {
+                ans = midW;
+                maxW = midW - 1;
+            }
+        }
+        return ans;
+    }
+};
