f33a283a63b5a1b0418669e6694642e7196d6383
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-07-09 20:47:42 +0530

    Time: 185 ms (18.82%) | Memory: 86.7 MB (6.47%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -7,8 +7,6 @@ public:
                 current_time > itr[0] ? current_time + itr[1] : itr[0] + itr[1];
             total += current_time - itr[0];
         }
-        double avg = 0;
-        avg = (double)total / (customers.size());
-        return avg;
+        return (double)total / (customers.size());
     }
 };
