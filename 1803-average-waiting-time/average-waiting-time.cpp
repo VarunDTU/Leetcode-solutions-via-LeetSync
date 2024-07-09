444947750193f73d65fd9f9123c5a8e0c089e6e3
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-07-09 20:49:04 +0530

    Time: 177 ms (22.35%) | Memory: 86.7 MB (6.47%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
-        double total = 0, current_time = 0;
+        double total = 0;
+        int current_time = 0;
         for (auto itr : customers) {
-            current_time =
-                current_time > itr[0] ? current_time + itr[1] : itr[0] + itr[1];
+            current_time = max(current_time, itr[0]) + itr[1];
             total += current_time - itr[0];
         }
         return (double)total / (customers.size());
