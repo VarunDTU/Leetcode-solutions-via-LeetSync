554a661b030b366eadf6958b8657af62ca05c368
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-07-22 00:28:58 +0530

    Time: 383 ms (37.57%) | Memory: 179.4 MB (21.87%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,25 +1,46 @@
 class NumArray {
 public:
-    vector<int>arr;
-    NumArray(vector<int>& nums) {
-        arr.resize(nums.size(),0);
-        arr[0]=nums[0];
-        for(int i=1;i<nums.size();i++)arr[i]=nums[i]+arr[i-1];
-    }
-    
-    void update(int index, int val) {
-        int temp=index==0?arr[index]:arr[index]-arr[index-1];
-        for(int i=index;i<arr.size();i++){
-            arr[i]-=temp;
-            arr[i]+=val;
-        }
-        
+    vector<int> arr;
+    int n;
+    void build(vector<int>& nums, int index, int left, int right) {
 
+        if (left == right) {
+            arr[index] = nums[left];
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        build(nums, 2 * index + 1, left, mid);
+        build(nums, 2 * index + 2, mid + 1, right);
+        arr[index] = arr[2 * index + 1] + arr[2 * index + 2];
     }
-    
-    int sumRange(int left, int right) {
-        return arr[right]-(left==0?0:arr[left-1]);
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        arr.resize(4 * n + 1);
+        build(nums, 0, 0, n - 1);
     }
+
+    int updatearr(int i, int left, int right, int index, int val) {
+        if (left == index && left == right) {
+            return arr[i] = val;
+        }
+        if (left > index || right < index)
+            return arr[i];
+        int mid = left + (right - left) / 2;
+        return arr[i] = updatearr(2 * i + 1, left, mid, index, val) +
+                        updatearr(2 * i + 2, mid + 1, right, index, val);
+    }
+    void update(int index, int val) { updatearr(0, 0, n - 1, index, val); }
+    int sumR(int left, int right, int index, int l, int r) {
+        if (left >= l && right <= r)
+            return arr[index];
+        if (left > r || right < l)
+            return 0;
+        int mid = left + (right - left) / 2;
+        int x = sumR(left, mid, 2 * index + 1, l, r),
+            y = sumR(mid + 1, right, 2 * index + 2, l, r);
+        return x + y;
+    }
+    int sumRange(int left, int right) { return sumR(0, n - 1, 0, left, right); }
 };
 
 /**
