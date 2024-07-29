200c99f186c95779ddf53ee2e2925693069ffdfb
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-07-29 20:15:29 +0530

    Time: 85 ms (20.92%) | Memory: 70.6 MB (24.88%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    
-    int helper(vector<int>&nums,int i,bool mustpick,vector<vector<int>>&dp){
-         ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        if(i==nums.size())return mustpick?0:-1e9;
-        if(dp[i][mustpick]!=-1e9)return dp[i][mustpick];
-        if(mustpick)return dp[i][mustpick]= max(0,nums[i]+helper(nums,i+1,true,dp));
-        return dp[i][mustpick]=max(helper(nums,i+1,false,dp),nums[i]+helper(nums,i+1,true,dp));
-    }
     int maxSubArray(vector<int>& nums) {
-         ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        vector<vector<int>>dp(nums.size()+1,vector<int>(2,-1e9));
-        return helper(nums,0,false,dp);
+        int ans=-1e9,currentsum=0;
+        for(auto itr:nums){
+            currentsum+=itr;
+            if(currentsum>ans){
+                ans=currentsum;
+            }
+            if(currentsum<0){
+                currentsum=0;
+            }
+
+        }
+        return ans;
     }
 };
