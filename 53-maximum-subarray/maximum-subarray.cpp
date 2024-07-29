28be036b958219bb44a020e31301b85420415dc9
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-07-29 20:16:36 +0530

    Time: 34 ms (99.50%) | Memory: 70.6 MB (10.56%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
         int ans=-1e9,currentsum=0;
         for(auto itr:nums){
             currentsum+=itr;
