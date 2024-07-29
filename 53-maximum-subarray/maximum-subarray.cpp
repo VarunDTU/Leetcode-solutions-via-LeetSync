fe18188eb6e9d361fdcf7a7c20433e6af1fd4f84
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-07-29 20:05:01 +0530

    Time: 164 ms (5.07%) | Memory: 120.8 MB (5.01%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    
+    int helper(vector<int>&nums,int i,bool mustpick,vector<vector<int>>&dp){
+        if(i==nums.size())return mustpick?0:-1e9;
+        if(dp[i][mustpick]!=-1e9)return dp[i][mustpick];
+        if(mustpick)return dp[i][mustpick]= max(0,nums[i]+helper(nums,i+1,true,dp));
+        return dp[i][mustpick]=max(helper(nums,i+1,false,dp),nums[i]+helper(nums,i+1,true,dp));
+    }
+    int maxSubArray(vector<int>& nums) {
+        vector<vector<int>>dp(nums.size()+1,vector<int>(2,-1e9));
+        return helper(nums,0,false,dp);
+    }
+};
