1a8040b8b1956d3bb0726bf155b2b2ae18391ed4
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-08-07 15:58:42 +0530

    Time: 58 ms (90.29%) | Memory: 184.4 MB (9.34%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    bool helper(vector<int>& nums, int &sum, int curr_sum, int i) {
-        if (curr_sum == sum)
+    bool helper(vector<int>& nums, int sum, int i) {
+        if (!sum)
             return true;
-        if (i == nums.size() || curr_sum > sum)
+        if (i == nums.size() || sum < 0)
             return false;
-        if (dp[i][curr_sum] != -1)
-            return dp[i][curr_sum];
-        return dp[i][curr_sum] = helper(nums, sum, curr_sum + nums[i], i + 1) ||
-                                 helper(nums, sum, curr_sum, i + 1);
+        if (dp[i][sum] != -1)
+            return dp[i][sum];
+        return dp[i][sum] = helper(nums, sum - nums[i], i + 1) ||
+                            helper(nums, sum, i + 1);
     }
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
@@ -17,6 +17,6 @@ public:
             return false;
         dp.resize(nums.size() + 1, vector<int>(sum + 1, -1));
         sum /= 2;
-        return helper(nums, sum, 0, 0);
+        return helper(nums, sum, 0);
     }
 };
