364388911a0e72cbfa1aa660702d78fcac2cdcbd
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-08-11 13:10:17 +0530

    Time: 9 ms (29.66%) | Memory: 15.8 MB (18.04%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,38 +1,40 @@
 class Solution {
-    vector<vector<string>>nQueens;
-    void helper(int row,vector<string>pos){
-        if(row==pos.size()){
+    vector<vector<string>> nQueens;
+    void helper(int row, vector<string> pos) {
+        if (row == pos.size()) {
             nQueens.push_back(pos);
         }
-        for(int i=0;i<pos.size();i++){
-            if(isValid(pos,row,i)){
-            pos[row][i]='Q';
-                helper(row+1,pos);
-            pos[row][i]='.';   
+        for (int i = 0; i < pos.size(); i++) {
+            if (isValid(pos, row, i)) {
+                pos[row][i] = 'Q';
+                helper(row + 1, pos);
+                pos[row][i] = '.';
             }
         }
     }
-    bool isValid(vector<string>board,int x,int y){
-        //row is already unique
-        for(int i=0;i<x;i++){
-            if(board[i][y]=='Q')return false;
+    bool isValid(vector<string> &board, int &x, int &y) {
+        // row is already unique
+        for (int i = 0; i < x; i++) {
+            if (board[i][y] == 'Q')
+                return false;
         }
-        
-        for(int i=x,j=y;j>=0&&i>=0;i--,j--){
-            if(board[i][j]=='Q')return false;
-        }
-        for(int i=x,j=y;j>=0&&i>=0;i--,j++){
-            if(board[i][j]=='Q')return false;
-        }
-    return true;
 
+        for (int i = x, j = y; j >= 0 && i >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        for (int i = x, j = y; j >= 0 && i >= 0; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        return true;
     }
-public:
 
+public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<string>chessboard(n,string(n,'.'));
-        nQueens={};
-        helper(0,chessboard);
+        vector<string> chessboard(n, string(n, '.'));
+        nQueens = {};
+        helper(0, chessboard);
         return nQueens;
     }
 };
