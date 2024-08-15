6a7aa2d95f17844436d0af50e01528da2f61797b
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-08-15 22:10:23 +0530

    Time: 254 ms (20.84%) | Memory: 32.4 MB (17.64%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -4,7 +4,7 @@ public:
         map<char,int>mp;
         for(auto itr:s)mp[itr]++;
         int ans=0;
-        for(auto itr:mp){
+        for(auto &itr:mp){
             if(itr.second%2)ans+=itr.second-1;
             else ans+=itr.second-2;
         }
