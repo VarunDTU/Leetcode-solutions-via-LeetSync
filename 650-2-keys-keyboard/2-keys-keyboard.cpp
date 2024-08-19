1200a3967837a0c9ee7377197b80bbffba04e9fb
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-08-19 10:05:57 +0530

    Time: 17 ms (31.47%) | Memory: 157.2 MB (8.56%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,12 +1,14 @@
 class Solution {
-    int helper(int n,int l ,int i){
+    int helper(int n,int l ,int i,vector<vector<int>>&dp){
         if(i>n)return 1e9;
         if(i==n)return 1;
-        return min(2+helper(n,i,i+i),1+helper(n,l,i+l));
+        if(dp[i][l]!=-1)return dp[i][l];
+        return dp[i][l]= min(2+helper(n,i,i+i,dp),1+helper(n,l,i+l,dp));
     }
 public:
     int minSteps(int n) {
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
         if(n<=1)return 0;
-        return helper(n,1,1);
+        return helper(n,1,1,dp);
     }
 };
