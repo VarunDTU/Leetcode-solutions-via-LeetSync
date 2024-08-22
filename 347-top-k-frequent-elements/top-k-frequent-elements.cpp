f2b1cfd9dc695f9af73ac1d36a9b7610b1b04257
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-08-22 21:54:05 +0530

    Time: 12 ms (61.95%) | Memory: 17.3 MB (55.69%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         priority_queue<pair<int,int>>q;
-        map<int,int>mp;
+        unordered_map<int,int>mp;
         for(auto itr:nums)mp[itr]++;
 
         for(auto itr:mp)q.push({itr.second,itr.first});
