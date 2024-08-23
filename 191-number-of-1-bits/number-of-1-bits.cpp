766fd5a32b3ef1bc8ea0aa0e5874345a617ef0a2
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-08-23 16:42:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (26.37%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,7 +3,8 @@ public:
     int hammingWeight(uint32_t n) {
         int ans=0;
         for(int i=0;i<32;i++){
-            ans+=((n>>i)&1);
+            if((n>>i)&1)ans++;
+        
         }
         return ans;
     }
