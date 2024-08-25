7031cc73f951ecaf9080c0492a372b246d9520fb
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-08-25 22:14:06 +0530

    Time: 276 ms (14.13%) | Memory: 138.3 MB (13.88%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    void helper(vector<vector<int>>&ans,int n,int i,int k,vector<int>temp){
+            if(n-k<0)return ;
+            if(k==0){
+                ans.push_back(temp);
+                return;
+            }
+
+            for(int j=i;j<=n;j++){
+                temp.push_back(j);
+                helper(ans,n,j+1,k-1,temp);
+                temp.pop_back();
+            }
+            return ;
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>ans;
+        helper(ans,n,1,k,{});
+        return ans;
+    }
+};
