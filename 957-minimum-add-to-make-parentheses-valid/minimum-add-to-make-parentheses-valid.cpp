d26a7ed4963a03d094b2ae3c390e0c8c8a46600b
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-09-01 19:30:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (50.55%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int st=0;
-        int count=0;
-        for(auto &itr:s){
+        int st=0, count=0;
+        for(int itr:s){
             if(itr=='(')st++;
             else if(st>0)st--;
             else count++;
