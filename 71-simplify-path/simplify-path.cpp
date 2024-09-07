d9a80dc8b0a52d8a554b617232511170ce775d9a
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-09-07 12:41:54 +0530

    Time: 5 ms (52.23%) | Memory: 9.8 MB (92.01%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        string temp = "";
+        for (int i = 0; i < path.size(); i++) {
+            if (path[i] != '/') {
+              
+                while (i < path.size() && path[i] != '/') {
+                    temp += path[i];
+                    i++;
+                }
+                if (temp == "..") {
+                    if (!st.empty())
+                        st.pop_back();
+                } else if (temp != ".")
+                    st.push_back(temp);
+                temp = "";
+            }
+        }
+        
+        for (int i = 0; i < st.size(); i++) {
+            temp += '/';
+            temp += st[i];
+        }
+     
+        return temp.size()==0?"/":temp;
+    }
+};
