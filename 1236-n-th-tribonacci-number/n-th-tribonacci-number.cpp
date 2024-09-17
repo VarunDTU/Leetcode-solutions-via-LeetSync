4ffd02f26d11998c7487097aede3255e29a40ffc
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-09-17 20:12:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (15.99%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    vector<int>dp;
+    int helper(int n){
+        if(n==0)return 0;
+        if(n<=2)return 1;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=helper(n-1)+helper(n-2)+helper(n-3);
+    }
+public:
+    int tribonacci(int n) {
+        dp.resize(n+1,-1);
+        return helper(n);
+    }
+};
