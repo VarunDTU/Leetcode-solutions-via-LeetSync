5258e9fdcf77a435f0ced09e5f4342e8fedbbe98
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-09-25 22:16:19 +0530

    Time: 786 ms (28.13%) | Memory: 291 MB (44.63%) - LeetSync

---
+++ 3541-report-spam-message/report-spam-message.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& ban) {
+        set<string>s(ban.begin(),ban.end());
+        int count=0;
+        for(auto itr:message){
+            if(s.find(itr)!=s.end())count++;
+            if(count==2)return true;
+        }
+        return false;
+    }
+};
