00b064e42d2382550d44817e916fb4223002714e
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-09-25 19:58:18 +0530

    Time: 221 ms (87.72%) | Memory: 160.9 MB (51.88%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,32 +1,39 @@
 
 class Solution {
-     class Node{
-        public:
-            Node* digits[10];   
+    class Node {
+    public:
+        Node* digits[10];
     };
-    void add(Node* head, string t){
+    void add(Node* head, string t) {
         Node* ptr = head;
-        for(auto c: t){
-            if(!ptr->digits[c - '0']) ptr->digits[c - '0'] = new Node();
-            ptr = ptr->digits[c-'0'];
+        for (auto c : t) {
+            if (!ptr->digits[c - '0'])
+                ptr->digits[c - '0'] = new Node();
+            ptr = ptr->digits[c - '0'];
         }
     }
-    int check(Node *head,string word){
-        int count=0;
-        for(auto itr:word){
-            if(head->digits[itr-'0'])count++;
-            else break;
-            head=head->digits[itr-'0'];
+    int check(Node* head, string word) {
+        int count = 0;
+        for (auto itr : word) {
+            if (head->digits[itr - '0'])
+                count++;
+            else
+                break;
+            head = head->digits[itr - '0'];
         }
         return count;
     }
+
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        if(arr1.size()>arr2.size())return longestCommonPrefix(arr2,arr1);
-        Node *head=new Node();
-        for(auto itr:arr1)add(head,to_string(itr));
-        int ans=0;
-        for(auto itr:arr2)ans=max(ans,check(head,to_string(itr)));
+        if (arr1.size() > arr2.size())
+            return longestCommonPrefix(arr2, arr1);
+        Node* head = new Node();
+        for (auto itr : arr1)
+            add(head, to_string(itr));
+        int ans = 0;
+        for (auto itr : arr2)
+            ans = max(ans, check(head, to_string(itr)));
         return ans;
     }
 };
