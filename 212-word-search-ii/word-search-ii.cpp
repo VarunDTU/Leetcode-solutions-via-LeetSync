1d27a219d52476d7bdf715a5e3e158017768d462
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-09-27 20:15:28 +0530

    Time: 708 ms (61.95%) | Memory: 19.2 MB (63.54%) - LeetSync

--- 212-word-search-ii/word-search-ii.cpp
+++ 212-word-search-ii/word-search-ii.cpp
@@ -1,12 +1,10 @@
 class TrieNode {
-    public:
-    TrieNode* dic[26]={NULL};
+public:
+    TrieNode* dic[26] = {NULL};
     bool isEnd;
     TrieNode() {
         isEnd = false;
-        for (auto itr : dic)
-            itr = NULL;
-
+      
     }
 };
 
@@ -21,26 +19,28 @@ class Solution {
         }
         root->isEnd = true;
     }
-    const int row[4]={-1,0,1,0},col[4]={0,-1,0,1};
-    void dfs(vector<vector<char>>& board,vector<vector<int>>& vis,int i,int j,TrieNode* root,string temp,vector<string>&ans){
-        
-        if(!root)return ;
-        if(root->isEnd){
+    const int row[4] = {-1, 0, 1, 0}, col[4] = {0, -1, 0, 1};
+    void dfs(vector<vector<char>>& board, vector<vector<int>>& vis, int i,
+             int j, TrieNode* root, string temp, vector<string>& ans) {
+
+        if (!root)
+            return;
+        if (root->isEnd) {
             ans.push_back(temp);
-            root->isEnd=false;
+            root->isEnd = false;
         }
-        vis[i][j]=true;
-        for(int k=0;k<4;k++){
-            int newRow=i+row[k],newCol=j+col[k];
-            if(newRow<board.size()&&newCol<board[0].size()&&!vis[newRow][newCol]&&root->dic[board[newRow][newCol]-'a']){
-                dfs(board,vis,newRow,newCol,root->dic[board[newRow][newCol]-'a'],temp+board[newRow][newCol],ans);
+        vis[i][j] = true;
+        for (int k = 0; k < 4; k++) {
+            int newRow = i + row[k], newCol = j + col[k];
+            if (newRow < board.size() && newCol < board[0].size() &&
+                !vis[newRow][newCol] &&
+                root->dic[board[newRow][newCol] - 'a']) {
+                dfs(board, vis, newRow, newCol,
+                    root->dic[board[newRow][newCol] - 'a'],
+                    temp + board[newRow][newCol], ans);
             }
-            
         }
-        vis[i][j]=false;
-
-
-
+        vis[i][j] = false;
     }
 
 public:
@@ -51,22 +51,20 @@ public:
 
         for (auto itr : words)
             insert(itr, head);
-        
-        vector<vector<int>>vis(board.size(),vector<int>(board[0].size(),false));
-        vector<string>ans;
 
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                //if(head->dic[board[i][j]-'a']){};
+        vector<vector<int>> vis(board.size(),
+                                vector<int>(board[0].size(), false));
+        vector<string> ans;
+
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                // if(head->dic[board[i][j]-'a']){};
                 string temp;
-                temp+=board[i][j];
-                    dfs(board,vis,i,j,head->dic[board[i][j]-'a'],temp,ans);
-                
+                temp += board[i][j];
+                dfs(board, vis, i, j, head->dic[board[i][j] - 'a'], temp, ans);
             }
         }
 
         return ans;
-
-
     }
 };
