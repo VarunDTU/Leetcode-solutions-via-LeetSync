e1695a3e146ad433053e65c23fbdc45c1f049831
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-10-01 20:12:35 +0530

    Time: 61 ms (16.67%) | Memory: 50.2 MB (26.75%) - LeetSync

---
+++ 3292-earliest-second-to-mark-indices-i/earliest-second-to-mark-indices-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+bool helper(vector<int>& nums, vector<int>& c,int end){
+        map<int,int>mp;
+        for(int i=0;i<end;i++){
+            mp[c[i]]=i;
+        }
+        if(mp.size()!=nums.size())return false;
+        int power=0;
+        for(int i=0;i<end;i++){
+            if(i==mp[c[i]]){
+                if(power>=nums[c[i]-1])power-=nums[c[i]-1];
+                else return false;
+            }else power++;
+        }
+        return true;
+    }
+public:
+    int earliestSecondToMarkIndices(vector<int>& nums, vector<int>& c) {
+        int n=nums.size(),m=c.size();
+        int s=0,e=m+1;
+        while(s<e){
+            int mid=s+(e-s)/2;
+            if(helper(nums,c,mid)){
+                e=mid;
+            }
+            else s=mid+1;
+        }
+        return (e)==m+1?-1:e;
+    }
+};
