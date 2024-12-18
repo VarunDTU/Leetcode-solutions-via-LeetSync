1c1c176e9a7ff3def4e170927fd1cb57c0e98136
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-12-18 15:43:44 +0530

    Time: 19 ms (15.51%) | Memory: 16.8 MB (34.49%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int helper(string word1,string word2,int i,int j,vector<vector<int>>&dp){
+    int helper(string &word1,string &word2,int i,int j,vector<vector<int>>&dp){
         if(i==word1.size())return word2.size()-j;
         if(j==word2.size())return word1.size()-i;
         if(dp[i][j]!=-1)return dp[i][j];
