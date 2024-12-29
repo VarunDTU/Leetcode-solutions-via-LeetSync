86f11a0c09106316068bebc0e78b39078e61525f
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-12-29 14:36:08 +0530

    Time: 51 ms (33.87%) | Memory: 38.3 MB (22.31%) - LeetSync

---
+++ 1905-design-authentication-manager/design-authentication-manager.cpp
@@ -0,0 +1,44 @@
+class AuthenticationManager {
+    unordered_map<string, int> tokenStore;
+    multiset<int> tokenLifetimes;
+    int lifetime = 0;
+
+public:
+    AuthenticationManager(int timeToLive) { lifetime = timeToLive; }
+
+    void generate(string tokenId, int currentTime) {
+
+        tokenStore[tokenId] = currentTime + lifetime;
+        tokenLifetimes.insert(currentTime + lifetime);
+    }
+
+    void renew(string tokenId, int currentTime) {
+      
+        if (tokenStore.find(tokenId) == tokenStore.end())
+            return;
+        int oldTime = tokenStore[tokenId];
+        if(oldTime<=currentTime)return ;
+        tokenStore[tokenId] = currentTime + lifetime;
+        auto tokenTime = tokenLifetimes.find(oldTime);
+        tokenLifetimes.erase(tokenTime);
+        
+        tokenLifetimes.insert(currentTime + lifetime);
+    }
+
+    int countUnexpiredTokens(int currentTime) {
+        int ans=0;
+        for(auto itr: tokenLifetimes){
+            if(itr>currentTime)ans++;
+        }
+        cout<<endl;
+        return ans;
+    }
+};
+
+/**
+ * Your AuthenticationManager object will be instantiated and called as such:
+ * AuthenticationManager* obj = new AuthenticationManager(timeToLive);
+ * obj->generate(tokenId,currentTime);
+ * obj->renew(tokenId,currentTime);
+ * int param_3 = obj->countUnexpiredTokens(currentTime);
+ */
