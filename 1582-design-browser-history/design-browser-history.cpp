04ef2bc2c67a45b505f0b772c144654337afbe08
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2024-12-31 21:52:12 +0530

    Time: 108 ms (9.65%) | Memory: 62.7 MB (79.28%) - LeetSync

---
+++ 1582-design-browser-history/design-browser-history.cpp
@@ -0,0 +1,39 @@
+class BrowserHistory {
+    vector<string> history;
+    int curr;
+
+public:
+    BrowserHistory(string homepage) {
+        curr = 1;
+        history.push_back(homepage);
+    }
+
+    void visit(string url) {
+        history.erase(history.begin()+curr,history.end());
+        history.push_back(url);
+        curr++;
+    }
+
+    string back(int steps) {
+        curr -= steps;
+        cout<<curr<<" ";
+        curr = max(curr, 1);
+        cout<<curr<<endl;
+        cout<<history[curr-1];
+        return history[curr-1];
+    }
+
+    string forward(int steps) {
+
+        curr = min(curr + steps, (int)(history.size()));
+        return history[curr-1];
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
