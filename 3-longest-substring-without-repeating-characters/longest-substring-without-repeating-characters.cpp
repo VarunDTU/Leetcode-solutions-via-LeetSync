fa2d7df04b890dc1c68e2d02e4f6c77416793c75
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2025-01-12 19:55:58 +0530

    Time: 90 ms (10.53%) | Memory: 11.8 MB (70.20%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int>dic;
+        int st=0,e=0,n=s.size(),ans=0;
+
+        while(e<n){
+            dic[s[e]]++;
+            cout<<dic[s[e]];
+            while(dic[s[e]]!=1){
+               
+                dic[s[st]]--;
+                st++;
+            }
+            ans=max(ans,e-st+1);
+            e++;
+        }
+        return ans;
+
+    }
+};
