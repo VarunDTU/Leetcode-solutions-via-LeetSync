60601d944a33fb6668cf02d30146634ed7ea9b37
Varun Negi <91000991+VarunDTU@users.noreply.github.com>
2025-01-12 15:02:57 +0530

    Time: 19 ms (35.63%) | Memory: 74.5 MB (19.24%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    bool isVowelends(string s){
+    bool isVowelends(string &s){
          int n=s.size();
        return (s[0]=='a'||s[0]=='i'||s[0]=='e'||s[0]=='o'||s[0]=='u')&&
        (s[n-1]=='a'||s[n-1]=='i'||s[n-1]=='e'||s[n-1]=='o'||s[n-1]=='u');
